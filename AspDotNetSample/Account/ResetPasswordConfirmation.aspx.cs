386309ce4adba76ac2df243e5fd2e772b8da0e88
ke fu <ke fu>
2017-07-09 19:17:17 +0900

    プロジェクト ファイルを追加します。

---
+++ AspDotNetSample/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace AspDotNetSample.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ AspDotNetSample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspDotNetSample.Startup))]
+namespace AspDotNetSample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
